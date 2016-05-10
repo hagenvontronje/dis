07cac9b03eaf84eceec24f742227de1f5a93da9f
Julian Burkhart <julian.burkhart@informatik.uni-hamburg.de>
2016-05-10 20:43:25 +0200

    Adds more hibernate tests.

--- exercises/sheet02/src/org/dis/sheet02/dal/HibernateTest.java
+++ exercises/sheet02/src/org/dis/sheet02/dal/HibernateTest.java
@@ -3,23 +3,21 @@ package org.dis.sheet02.dal;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.util.Collection;
 import java.util.EnumSet;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
-import org.dis.sheet02.entities.Appartment;
 import org.dis.sheet02.entities.EstateAgent;
 import org.dis.sheet02.entities.House;
 import org.dis.sheet02.entities.Person;
-import org.dis.sheet02.entities.PurchaseContract;
-import org.dis.sheet02.entities.TenancyContract;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.mapping.PersistentClass;
+import org.hibernate.criterion.Projections;
 import org.hibernate.resource.transaction.spi.TransactionStatus;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.hibernate.tool.schema.TargetType;
@@ -41,42 +39,50 @@ public class HibernateTest {
 		new SchemaExport().create(EnumSet.of(TargetType.DATABASE), metadata);
 	}
 	
-	
-	@Test
-	public void test() {
+	public <T> void testEntity(	Class<T> entityClass,
+								Supplier<T> newEntitySupplier, 
+								Consumer<T> entityModificator,
+								Function<T, Integer> idSelector) {
 		Session session = null;
 		try {
 			session = sessionFactory.openSession();
 			assertTrue(session.isOpen());
 
-			Person person = new Person("Arthur", "Dent",
-					"Definitely not on earth");
-			assertTrue(person.getId() <= 0);
+			long originalCount = (long) session
+					.createCriteria(entityClass)
+					.setProjection(Projections.rowCount())
+					.uniqueResult();
+			
+			
+			T person = newEntitySupplier.get();
+			assertTrue(idSelector.apply(person) <= 0);
 
 			session.beginTransaction();
 			session.persist(person);
-			assertTrue(person.getId() > 0);
+			assertTrue(idSelector.apply(person) > 0);
 
-			person.setAddress("The whole wide universe");
-			session.flush();
-			session.evict(person);
+			long newCount = (long) session
+					.createCriteria(entityClass)
+					.setProjection(Projections.rowCount())
+					.uniqueResult();
+			assertTrue(newCount == originalCount + 1);
+			
+			entityModificator.accept(person);
+//			session.flush();
+//			session.evict(person);
 			session.getTransaction().commit();
 
 			session = sessionFactory.getCurrentSession();
 			assertTrue(session.isOpen());
 			session.beginTransaction();
 
-			@SuppressWarnings("unchecked")
-			Collection<Person> persons = session.createCriteria(Person.class)
-					.list();
-			System.out.println("Persons:");
-			for (Person p : persons)
-				System.out.printf(
-						"Person: id=%d, first_name=%s, last_name=%s, address=%s%n",
-						p.getId(), p.getFirstName(), p.getLastName(),
-						p.getAddress());
-			person = session.load(Person.class, person.getId());
+			person = session.load(entityClass, idSelector.apply(person));
 			session.delete(person);
+			newCount = (long) session
+					.createCriteria(entityClass)
+					.setProjection(Projections.rowCount())
+					.uniqueResult();
+			assertTrue(newCount == originalCount);
 			session.getTransaction().commit();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -88,6 +94,30 @@ public class HibernateTest {
 				session.getTransaction().rollback();
 			fail(e.getMessage());
 		}
+	}
+	
+	@Test
+	public void testPerson() {
+		testEntity(Person.class, 
+				() -> new Person("Arthur", "Testman", "Teststreet 123b"), 
+				(p) -> p.setFirstName("Bertha"), 
+				(p) -> p.getId());
+	}
+	
+	@Test
+	public void testEstateAgent() {
+		testEntity(EstateAgent.class, 
+				() -> new EstateAgent("Arthur Dent", "Not on earth", "adent", "adent"), 
+				(p) -> p.setName("A. Dent"), 
+				(p) -> p.getId());
+	}
+	
+	@Test 
+	public void testHouse() {
+		testEntity(House.class, 
+				() -> new House("Hamburg", "22347", "Somstreet", "123b", 150.32, 1, 120000.73, false, 0), 
+				(h) -> h.setCity("Triton 73b"),
+				(h) -> h.getId());
 	}
 
 }
