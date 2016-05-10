7dc086e05c634365c8443b01fd7bc812099b22a8
Julian Burkhart <julian.burkhart@informatik.uni-hamburg.de>
2016-05-10 20:43:09 +0200

    Adds missing entity annotation to EstateAgent.

--- exercises/sheet02/src/org/dis/sheet02/entities/EstateAgent.java
+++ exercises/sheet02/src/org/dis/sheet02/entities/EstateAgent.java
@@ -1,11 +1,13 @@
 package org.dis.sheet02.entities;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Entity
 @Table(name = "ESTATE_AGENT")
 public class EstateAgent {
 
